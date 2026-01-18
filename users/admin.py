d22c69e66d9b8eaba60b5ab78f149de81d7ad09e
CMW <cmwhknz@gmail.com>
2026-01-18 21:33:34 +0800

    Remove users

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-from .models import User
-
-class CustomUserAdmin(UserAdmin):
-    list_display = (['username','stripe_id'])
-
-admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    stripe_id = models.CharField(max_length=30, default = 'none')   
